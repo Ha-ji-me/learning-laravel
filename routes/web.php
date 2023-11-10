f1a129662f60b53347ff58c48dd4a8f23231c180
Ha-ji-me <hjmx3599@gmail.com>
2023-11-10 22:58:42 +0900

    ルーティング作成

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,6 @@ Route::get('/', function () {
 });
 
 Route::get('/tests', [TestController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{postId}', [PostController::class, 'show']);
+// Route::get('/posts/{postId}/edit', [PostController::class, 'edit']);
