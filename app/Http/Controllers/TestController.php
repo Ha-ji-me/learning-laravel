4c6012466de41dd73b67b5915c3cdc55cba7783a
Ha-ji-me <hjmx3599@gmail.com>
2023-11-10 22:55:38 +0900

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Test;
 
 class TestController extends Controller
 {
+    private $test;
+
+    public function __construct(Test $test) {
+        $this->test = $test;
+    }
+
     public function index() {
-        return view('test');
+        // return view('test');
+        return $this->test->returnTest();
     }
 }
