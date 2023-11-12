86005bb99e96ec806b0cfcfc9295edd0b16c2bb6
Ha-ji-me <hjmx3599@gmail.com>
2023-11-12 20:45:51 +0900

    コメント作成

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    private $comment;
+
+    public function __construct(Comment $comment)
+    {
+        $this->comment = $comment;
+    }
+
+    public function index()
+    {
+        return $this->comment->getAll();
+    }
+
+    public function show($postId, $commentId)
+    {
+        return $this->comment->getBy($postId, $commentId);
+    }
+
+
+    public function comments()
+    {
+        return $this->comment->test();
+    }
 }
--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    public function post() {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function getAll()
+    {
+        $comments = $this->with('post')->get();
+        return response()->json($comments);
+    }
+
+    public function getBy($postId, $commentId)
+    {
+        $comment = $this->with('post:id,title,content,user_id', 'post.user:id,name')->where([['post_id', $postId],['id', $commentId]])->get();
+        return response()->json($comment);
+    }
+
+
+    public function test()
+    {
+        $comments = $this->select('id','content','user_id','post_id')
+            ->with(
+                'post:id,title,content,user_id',
+                'post.user:id,name')
+            ->get();
+        return response()->json($comments);
+    }
 }
