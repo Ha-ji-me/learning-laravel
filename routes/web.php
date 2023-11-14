77af080b5719832b77e2e70b0a34c1296bf0d1dd
Ha-ji-me <hjmx3599@gmail.com>
2023-11-14 21:10:52 +0900

    内部でcommandを実行（ファサード）

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
+use Illuminate\Support\Facades\Artisan;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +25,11 @@ Route::get('/tests', [TestController::class, 'index']);
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/{postId}', [PostController::class, 'show']);
 // Route::get('/posts/{postId}/edit', [PostController::class, 'edit']);
+Route::get('/comments', [CommentController::class, 'comments']);
+Route::get('/posts/{postId}/comments', [CommentController::class, 'index']);
+Route::get('/posts/{postId}/comments/{commentId}', [CommentController::class, 'show']);
+Route::get('/commands', function() {
+    Artisan::call('hello:class', [
+        '--switch' => true,
+    ]);
+});
